e88fefc49a72c09801d9bd0f569333e6837268d9
Max Krummenacher <maxkrummenacher@bluewin.ch>
2023-06-10 11:31:56 +0200

    some progress

--- src/lib.rs
+++ src/lib.rs
@@ -2,84 +2,259 @@ use phases::{Array2d, NumAtom, NumC, System};
 use wasm_bindgen::prelude::*;
 mod utils;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
-}
-
 type Concentration = NumC<2>;
 type Atom = NumAtom<2>;
 
-type MediumSystem = System<Array2d<Atom, 128, 128>, [f32; 4]>;
+macro_rules! makemodel {
+    ($name:ident, $size:literal) => {
+        #[wasm_bindgen]
+        pub struct $name {
+            system: System<Array2d<Atom, $size, $size>, [f32; 4]>,
+            method: fn(&mut System<Array2d<Atom, $size, $size>, [f32; 4]>, f32) -> bool,
+            animation: Vec<[[u8; $size]; $size]>,
+            temp: Vec<f32>,
+            int_energy: Vec<f32>,
+            heat_capacity: Vec<f32>,
+        }
 
-#[wasm_bindgen]
-pub struct MediumModel {
-    system: MediumSystem,
-    method: fn(&mut MediumSystem, f32) -> bool,
-    animation: Vec<[[u8; 128]; 128]>,
+        #[wasm_bindgen]
+        impl $name {
+            pub fn new(
+                energies: Vec<f32>,
+                c_1: f64,
+                c_2: f64,
+                method: &str,
+                animation_capcity: u32,
+                log_capacity: u32,
+            ) -> Self {
+                utils::set_panic_hook();
+                let method = match method {
+                    "monte_carlo_swap" => System::monte_carlo_swap,
+                    "move_vacancy" => System::move_vacancy,
+                    _ => {
+                        alert(&format!(
+                            "method '{}' not found, using 'move_vacancy'",
+                            method
+                        ));
+                        System::move_vacancy
+                    }
+                };
+
+                let energies = [energies[0], energies[1], energies[2], energies[3]];
+                Self {
+                    system: System::new(energies, None, Concentration::new([c_1, c_2])),
+                    method,
+                    animation: Vec::with_capacity(animation_capcity as usize),
+                    temp: Vec::with_capacity(log_capacity as usize),
+                    int_energy: Vec::with_capacity(log_capacity as usize),
+                    heat_capacity: Vec::with_capacity(log_capacity as usize),
+                }
+            }
+
+            pub fn run_at_temp(
+                &mut self,
+                equilibrium_steps: u32,
+                measurement_steps: u32,
+                temp: f32,
+                frames: u32,
+            ) {
+                self.temp.push(temp);
+
+                for _ in 0..equilibrium_steps * $size * $size {
+                    (self.method)(&mut self.system, temp);
+                }
+
+                // variance after https://math.stackexchange.com/questions/20593/calculate-variance-from-a-stream-of-sample-values
+                let mut m_k = 0.0;
+                let mut m_k_1;
+                let mut v_k = 0.0;
+                let mut v_k_1;
+
+                for k in 1..=measurement_steps * $size * $size {
+                    (self.method)(&mut self.system, temp);
+                    let x_k = self.system.internal_energy();
+                    m_k_1 = m_k;
+                    v_k_1 = v_k;
+                    m_k = m_k_1 + (x_k - m_k_1) / k as f32;
+                    v_k = v_k_1 - (x_k - m_k_1) * (x_k - m_k);
+                    if k % measurement_steps / frames == 0 {
+                        self.animation.push(
+                            // SAFETY the implentation of NumAtom guarantees this is safe
+                            unsafe { std::mem::transmute(*self.system.return_state().grid) },
+                        )
+                    }
+                }
+                self.int_energy.push(m_k);
+                let variance = v_k / (measurement_steps * $size * $size) as f32;
+                self.heat_capacity.push(variance / temp / temp)
+            }
+        }
+
+        #[wasm_bindgen]
+        impl $name {
+            pub fn log_len(&self) -> u32 {
+                self.int_energy.len() as u32
+            }
+
+            pub fn int_energy_ptr(&self) -> *const f32 {
+                self.int_energy.as_ptr()
+            }
+            pub fn temp_ptr(&self) -> *const f32 {
+                self.temp.as_ptr()
+            }
+            pub fn heat_capacity_ptr(&self) -> *const f32 {
+                self.heat_capacity.as_ptr()
+            }
+        }
+
+        /// Animation stuff
+        #[wasm_bindgen]
+        impl $name {
+            pub fn width(&self) -> u32 {
+                $size
+            }
+
+            pub fn height(&self) -> u32 {
+                $size
+            }
+
+            pub fn anim_len(&self) -> u32 {
+                self.animation.len() as u32
+            }
+
+            pub fn anim_start_ptr(&self) -> *const u8 {
+                self.animation[0].as_ptr().cast()
+            }
+
+            pub fn anim_frame_ptr(&self, idx: usize) -> *const u8 {
+                self.animation[idx].as_ptr().cast()
+            }
+
+            pub fn anim_last_frame_ptr(&self) -> *const u8 {
+                self.animation[self.animation.len() - 1].as_ptr().cast()
+            }
+        }
+    };
 }
 
+makemodel!(BigModel, 256);
+makemodel!(MediumModel, 128);
+makemodel!(SmallModel, 64);
 #[wasm_bindgen]
-impl MediumModel {
-    pub fn new(energies: Vec<f32>, c_1: f64, c_2: f64, method: &str) -> Self {
+pub struct TinyModel {
+    system: System<Array2d<Atom, 32, 32>, [f32; 4]>,
+    method: fn(&mut System<Array2d<Atom, 32, 32>, [f32; 4]>, f32) -> bool,
+    animation: Vec<[[u8; 32]; 32]>,
+    temp: Vec<f32>,
+    int_energy: Vec<f32>,
+    heat_capacity: Vec<f32>,
+}
+#[wasm_bindgen]
+impl TinyModel {
+    pub fn new(
+        energies: Vec<f32>,
+        c_1: f64,
+        c_2: f64,
+        method: &str,
+        animation_capcity: u32,
+        log_capacity: u32,
+    ) -> Self {
+        utils::set_panic_hook();
         let method = match method {
-            "move_vacancy" => MediumSystem::move_vacancy,
+            "monte_carlo_swap" => System::monte_carlo_swap,
+            "move_vacancy" => System::move_vacancy,
             _ => {
                 alert(&format!(
-                    "method '{}', not found using 'move_vacancy'",
+                    "method '{}' not found, using 'move_vacancy'",
                     method
                 ));
-                MediumSystem::move_vacancy
+                System::move_vacancy
             }
         };
-
         let energies = [energies[0], energies[1], energies[2], energies[3]];
         Self {
-            system: MediumSystem::new(energies, None, Concentration::new([c_1, c_2])),
+            system: System::new(energies, None, Concentration::new([c_1, c_2])),
             method,
-            animation: Vec::new(),
+            animation: Vec::with_capacity(animation_capcity as usize),
+            temp: Vec::with_capacity(log_capacity as usize),
+            int_energy: Vec::with_capacity(log_capacity as usize),
+            heat_capacity: Vec::with_capacity(log_capacity as usize),
         }
     }
-
-    pub fn take_steps(&mut self, steps: u32, temp: f32) {
-        for _ in 0..steps {
+    pub fn run_at_temp(
+        &mut self,
+        equilibrium_steps: u32,
+        measurement_steps: u32,
+        temp: f32,
+        frames: u32,
+    ) {
+        self.temp.push(temp);
+        for _ in 0..equilibrium_steps * 32 * 32 {
             (self.method)(&mut self.system, temp);
-            // SAFETY the implentation of NumAtom guarantees this is safe
-            unsafe {
+        }
+        let mut m_k = 0.0;
+        let mut m_k_1;
+        let mut v_k = 0.0;
+        let mut v_k_1;
+        for k in 1..=measurement_steps * 32 * 32 {
+            (self.method)(&mut self.system, temp);
+            let x_k = self.system.internal_energy();
+            m_k_1 = m_k;
+            v_k_1 = v_k;
+            m_k = m_k_1 + (x_k - m_k_1) / k as f32;
+            v_k = v_k_1 - (x_k - m_k_1) * (x_k - m_k);
+            if k % measurement_steps / frames == 0 {
                 self.animation
-                    .push(std::mem::transmute(*self.system.return_state().grid))
+                    .push(unsafe { std::mem::transmute(*self.system.return_state().grid) })
             }
         }
+        self.int_energy.push(m_k);
+        let variance = v_k / (measurement_steps * 32 * 32) as f32;
+        self.heat_capacity.push(variance / temp / temp)
     }
 }
-
-/// Animation stuff
 #[wasm_bindgen]
-impl MediumModel {
+impl TinyModel {
+    pub fn log_len(&self) -> u32 {
+        self.int_energy.len() as u32
+    }
+    pub fn int_energy_ptr(&self) -> *const f32 {
+        self.int_energy.as_ptr()
+    }
+    pub fn temp_ptr(&self) -> *const f32 {
+        self.temp.as_ptr()
+    }
+    pub fn heat_capacity_ptr(&self) -> *const f32 {
+        self.heat_capacity.as_ptr()
+    }
+}
+#[doc = " Animation stuff"]
+#[wasm_bindgen]
+impl TinyModel {
     pub fn width(&self) -> u32 {
-        128
+        32
     }
-
     pub fn height(&self) -> u32 {
-        128
+        32
     }
-
     pub fn anim_len(&self) -> u32 {
         self.animation.len() as u32
     }
-
-    pub fn anim_ptr(&self) -> *const u8 {
+    pub fn anim_start_ptr(&self) -> *const u8 {
         self.animation[0].as_ptr().cast()
+    }
+    pub fn anim_frame_ptr(&self, idx: usize) -> *const u8 {
+        self.animation[idx].as_ptr().cast()
+    }
+    pub fn anim_last_frame_ptr(&self) -> *const u8 {
+        self
+        .animation[self.animation.len() - 1]
+        .as_ptr()
+        .cast()
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,46 +1,34 @@
-import { MediumModel } from "thermo-online";
+import { TinyModel, SmallModel, MediumModel } from "thermo-online";
 import { memory } from "thermo-online/thermo_online_bg";
 
-const model = MediumModel.new([1.0, 0.0, 0.0, 1.0], 1.0, 1.0, "move_vacancy");
-
-
-
-model.take_steps(50, 0.7);
-
-
 const CELL_SIZE = 5;
+const TEMP_STEPS = 50;
+const START_TEMP = 1.0;
+const E_STEPS = 100;
+const M_STEPS = 100;
+const N_FRAMES = 3;
+
+const model = TinyModel.new([-1.0, 1.0, 1.0, 1.0], 1.0, 1.0, "move_vacancy", TEMP_STEPS * N_FRAMES, TEMP_STEPS);
 const width = model.width();
 const height = model.height();
-const animationLen = model.anim_len();
+
 const canvas = document.getElementById("main_animation");
 canvas.height = CELL_SIZE * height;
 canvas.width = CELL_SIZE * width;
-
-
 const ctx = canvas.getContext('2d');
 
 
-var currentFrame = 0;
-const renderLoop = () => {
-    currentFrame = (currentFrame + 1) % animationLen
-    drawGrid(currentFrame)
-
-    requestAnimationFrame(renderLoop);
-}
-
-const getIndex = (frame, row, column) => {
-    return frame * width * height + row * width + column;
+const getIndex = (row, column) => {
+    return row * width + column;
 };
 
-const drawGrid = (frame) => {
-    const animPtr = model.anim_ptr();
-    const animation = new Uint8Array(memory.buffer, animPtr, width * height * animationLen);
+const drawGrid = (array) => {
     ctx.beginPath();
 
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
-            const idx = getIndex(frame, row, col)
-            ctx.fillStyle = animation[idx] === 0 ? "orange" : "red"
+            const idx = getIndex(row, col)
+            ctx.fillStyle = array[idx] === 0 ? "orange" : "red"
 
             ctx.fillRect(
                 col * CELL_SIZE,
@@ -52,5 +40,34 @@ const drawGrid = (frame) => {
     }
     ctx.stroke()
 }
+
+var temp_step = TEMP_STEPS;
+const coolingLoop = () => {
+    temp_step -= 1
+    const temp = START_TEMP * (temp_step / (TEMP_STEPS - 1))
+    model.run_at_temp(E_STEPS, M_STEPS, temp, 1)
+    const frame = new Uint8Array(memory.buffer, model.anim_start_ptr(), width * height);
+    console.log(temp)
+    console.log(temp_step)
+    drawGrid(frame)
+    if (temp_step === 0) {
+        return;
+    }
+    requestAnimationFrame(coolingLoop)
+}
+
+var currentFrame = 0;
+const renderLoop = () => {
+    {
+        let animation = new Uint8Array(memory.buffer, model.anim_frame_ptr(currentFrame), width * height);
+        drawGrid(animation)
+        currentFrame = (currentFrame + 1) % animationLen
+    }
+    requestAnimationFrame(renderLoop);
+}
+
+
+coolingLoop()
+const animationLen = model.anim_len();
 
 renderLoop()
