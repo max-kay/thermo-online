830858857d54088bc6543ce1ffc4b1b61cb5a2f8
Max Krummenacher <maxkrummenacher@bluewin.ch>
2023-06-19 16:22:10 +0200

    added icon

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'styles.css'])
+    new CopyWebpackPlugin(['index.html', 'styles.css', 'icon.webp'])
   ],
 };
